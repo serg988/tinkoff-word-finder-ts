1808b00da66c092113f38f359a0e0d0301a6e73a
serg988 <serg988@gmail.com>
2022-11-20 18:27:24 +0300

    fixes + removed duplicates from dict

--- src/components/FilteredList.tsx
+++ src/components/FilteredList.tsx
@@ -13,24 +13,24 @@ const FilteredList: React.FC<IFilteredList> = ({ term }) => {
   }, [])
 
   useEffect(() => {
-    //----------------L I P------------------
+    // --------NOT PRESENTED LETTERS
+    if (term?.npl && term?.npl.length > 0) {
+      const nplWithoutDots = term.npl.filter((l) => l !== '.')
+     
 
-    let expression = '.....'
-
-    if (term?.lip) {
-      if (/[а-я]/.test(term?.lip.join(''))) {
-        expression = term?.lip.join('')
-      }
+      nplWithoutDots.forEach((element: string) => {
+        setWordsList((prevState) =>
+          prevState.filter((word) => word.indexOf(element) === -1)
+        )
+      })
     }
-
-    const regexObj = new RegExp(expression)
-
-    setWordsList((prevState) => prevState.filter((word) => regexObj.test(word)))
-
     // -------------PRESENTED LETTERS-------------
 
+    //  const differencePl = nplWithoutDots.filter((x) => !term.pl.includes(x))
+    //  const differenceLip = differencePl.filter((x) => !term.lip.includes(x))
+
     if (term?.pl && term?.pl.length > 0) {
-      let exp = '......'
+      let exp = '.....'
 
       if (term?.pl) {
         if (/[а-я]/.test(term?.pl.join(''))) {
@@ -44,25 +44,29 @@ const FilteredList: React.FC<IFilteredList> = ({ term }) => {
         prevState.filter((word) => !regexObj.test(word))
       )
       const plWithoutDots = term.pl.filter((l) => l !== '.')
-      plWithoutDots.forEach((element: string) => {
+       const differencePl = plWithoutDots.filter((x) => !term.pl.includes(x))
+       const differenceLip = differencePl.filter((x) => !term.lip.includes(x))
+
+      differenceLip.forEach((element: string) => {
         setWordsList((prevState) =>
           prevState.filter((word) => word.includes(element))
         )
       })
     }
 
-    // --------NOT PRESENTED LETTERS
-    if (term?.npl && term?.npl.length > 0) {
-      const nplWithoutDots = term.npl.filter((l) => l !== '.')
-      const differencePl = nplWithoutDots.filter((x) => !term.pl.includes(x))
-      const differenceLip = differencePl.filter((x) => !term.lip.includes(x))
+    //----------------L I P------------------
 
-      differenceLip.forEach((element: string) => {
-        setWordsList((prevState) =>
-          prevState.filter((word) => word.indexOf(element) === -1)
-        )
-      })
+    let expression = '.....'
+
+    if (term?.lip) {
+      if (/[а-я]/.test(term?.lip.join(''))) {
+        expression = term?.lip.join('')
+      }
     }
+
+    const regexObj = new RegExp(expression)
+
+    setWordsList((prevState) => prevState.filter((word) => regexObj.test(word)))
     //--------------------------------------------
   }, [term])
 
@@ -74,17 +78,17 @@ const FilteredList: React.FC<IFilteredList> = ({ term }) => {
 
   let content: string[]
   if (wordsList.length <= 300) {
-    content= wordsList
+    content = wordsList
   }
   // Shuffle array
   const shuffled = wordsList.sort(() => 0.5 - Math.random())
 
   // Get sub-array of first n elements after shuffled
-  content = alphabeticalOrder(shuffled.slice(0, 300)) 
+  content = alphabeticalOrder(shuffled.slice(0, 300))
 
   return (
     <div className='filteredList'>
-      {content.slice(0, 300).map((w) => {
+      {content.slice(0, 30000).map((w) => {
         return <span key={w}>{w}, </span>
       })}
     </div>
